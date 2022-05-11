f08dffd04690fa634bf3847f58e6c46443af5ce7
Guillaume SOULAT <gsoulat31@gmail.com>
2022-05-11 11:44:49 +0200

    streamlit

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
-from App.model import *
+from model import *
 import streamlit as st
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-import time
-import utils as utl
-from App.views import home,about,analysis,options,configuration
+from streamlit_option_menu import option_menu
+
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 Sessions = sessionmaker(bind=engine)
 sess = Sessions()
-st.set_page_config(layout="wide", page_title='Navbar sample')
-st.set_option('deprecation.showPyplotGlobalUse', False)
-utl.inject_custom_css()
-utl.navbar_component()
 
-def navigation():
-    route = utl.get_current_route()
-    if route == "home":
-        home.load_view()
-    elif route == "about":
-        about.load_view()
-    elif route == "analysis":
-        analysis.load_view()
-    elif route == "options":
-        options.load_view()
-    elif route == "configuration":
-        configuration.load_view()
-    elif route == None:
-        home.load_view()
-        
-navigation()
+with st.sidebar:
+    connected = False
+    if connected:
+        print(connected)
+        selected = option_menu("Main Menu", ["Home", "Patient", "Information", 'Settings'], icons=['house', '', 'card-text','gear'], menu_icon="cast", default_index=1)
+    else:
+        print("not connected")
+        selected = option_menu("Main Menu", ["Home","Sign-in", "Sign-up"], icons=['house', "person", "pen"], menu_icon="cast", default_index=1)
+
+if selected == 'Home':
+    st.title('coucou home')
+    st.balloons()
+    
+elif (selected == 'Patient') & (connected==True):
+    st.title('coucou Patient')
+    
+    
+elif (selected == 'Information') & (connected==True):
+    st.title('coucou Information')
+    
+elif (selected == 'Sign-in') & (connected==False):
+    st.title("Vous n'etes pas connecté bande de batard")
+    
+elif (selected == 'Sign-up') & (connected==False):
+    st.title("Enregistrer tete d'alien")
